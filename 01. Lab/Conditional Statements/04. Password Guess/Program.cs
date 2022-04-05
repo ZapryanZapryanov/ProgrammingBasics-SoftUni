a0725568df9fc24a6eded750e90eb20204400161
zaprian1223 <zaprian1223@gmail.com>
2022-04-05 20:32:35 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements/04. Password Guess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Password_Guess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string passrow = Console.ReadLine();
+            bool pass = passrow == ("s3cr3t!P@ssw0rd");
+            if (pass)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
