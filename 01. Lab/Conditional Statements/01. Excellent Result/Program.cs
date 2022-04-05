4576ae38da5a2e9573cb6ba8c60492327b6f5c04
zaprian1223 <zaprian1223@gmail.com>
2022-04-05 20:31:36 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements/01. Excellent Result/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            bool flag = num >= 5.50;
+
+            if (flag)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
