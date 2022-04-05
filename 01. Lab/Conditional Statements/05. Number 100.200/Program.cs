2774b3849edc82f6774eb563be1ae2440bd6eb66
zaprian1223 <zaprian1223@gmail.com>
2022-04-05 20:32:54 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements/05. Number 100.200/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Number_100._200
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chek = int.Parse(Console.ReadLine());
+            bool chekDown = chek < 100;
+
+
+            bool chekGo = chek > 200;
+            if (chekDown)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if (chekGo)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+            else
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+        }
+    }
+}
