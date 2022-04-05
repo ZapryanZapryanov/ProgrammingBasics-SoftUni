73b20a53aba44124fd4d5d3375b3ed13a961e10d
zaprian1223 <zaprian1223@gmail.com>
2022-04-05 20:33:14 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements/06. Speed Info/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06._Speed_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double speedInfo = double.Parse(Console.ReadLine());
+
+
+            bool slow = speedInfo <= 10;
+            bool average = speedInfo <= 50;
+            bool fast = speedInfo <= 150;
+            bool ultraFast = speedInfo <= 1000;
+            if (slow)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (average)
+            {
+                Console.WriteLine("average");
+            }
+            else if (fast)
+            {
+                Console.WriteLine("fast");
+            }
+            else if (ultraFast)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+
+        }
+    }
+}
