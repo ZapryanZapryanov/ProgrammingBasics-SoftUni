d8c190822a0c8222333a266c2a9a444f42604076
zaprian1223 <zaprian1223@gmail.com>
2022-04-06 21:33:58 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements Advanced/12. Trade Commissions/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace _12._Trade_Commissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+
+            double price = 0;
+
+
+            if (city == "Sofia" && sales >= 10 % 2)
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    price = 0.05;
+                    Console.WriteLine($"{(sales * price):f2}");
+                }
+                else if (sales <= 1000)
+                {
+                    price = 0.07;
+                    Console.WriteLine($"{(sales * price):f2}");
+                }
+                else if (sales <= 10000)
+                {
+                    price = 0.08;
+                    Console.WriteLine($"{(sales * price):f2}");
+                }
+                else if (sales > 10000)
+                {
+                    price = 0.12;
+                    Console.WriteLine($"{(sales * price):f2}");
+                }
+
+            }
+            else if (city == "Plovdiv" && sales >= 10 % 2)
+            {
+
+                if (sales <= 500)
+                {
+                    price = 0.055;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+                else if (sales > 0 && sales <= 1000)
+                {
+                    price = 0.08;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+                else if (sales <= 10000)
+                {
+                    price = 0.12;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+                else if (sales > 10000)
+                {
+                    price = 0.145;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+                else if (sales == -20)
+                {
+                    Console.WriteLine("error");
+                }
+
+
+            }
+            else if (city == "Varna" && sales >= 10 % 2)
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    price = 0.045;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+                else if (sales <= 1000)
+                {
+                    price = 0.075;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+                else if (sales <= 10000)
+                {
+                    price = 0.1;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+                else
+                {
+                    price = 0.13;
+                    Console.WriteLine($"{sales * price:f2}");
+                }
+
+            }
+
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+        }
+    }
+}
