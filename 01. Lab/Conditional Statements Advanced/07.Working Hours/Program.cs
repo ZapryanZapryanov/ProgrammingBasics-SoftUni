846f9c2538d9743a2f480bd36ee6159b70ac5284
zaprian1223 <zaprian1223@gmail.com>
2022-04-06 21:32:10 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements Advanced/07.Working Hours/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            string dayWeek = Console.ReadLine();
+
+            if (hours <= 18 && hours >= 10)
+            {
+                if (dayWeek == "Tuesday" || dayWeek == "Wednesday" || dayWeek == "Monday"
+                    || dayWeek == "Friday" || dayWeek == "Saturday" || dayWeek == "Thursday")
+
+                    Console.WriteLine("open");
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
