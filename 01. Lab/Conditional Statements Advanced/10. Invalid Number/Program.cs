08d1052dcf970afe61e2f37de820dd4e9011123c
zaprian1223 <zaprian1223@gmail.com>
2022-04-06 21:33:12 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements Advanced/10. Invalid Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10._Invalid_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            bool flag = num == 0;
+
+            if (!flag)
+            {
+                if (num < 100 || num > 200)
+                {
+                    Console.WriteLine("invalid");
+                }
+
+            }
+        }
+    }
+}
