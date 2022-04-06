88125aadf55bbe144e58d2d686d27c2efd9530b9
zaprian1223 <zaprian1223@gmail.com>
2022-04-06 21:30:36 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements Advanced/03. Animal Type/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03._Animal_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String animal = Console.ReadLine();
+
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+
+                case "crocodile":
+                case "snake":
+                case "tortoise":
+                    Console.WriteLine("reptile");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
