ae294555402a1962d3669a73a8807cfe44c51dd2
zaprian1223 <zaprian1223@gmail.com>
2022-04-06 21:31:02 +0300

    Added a solution

---
+++ 01. Lab/Conditional Statements Advanced/04. Personal Titles/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _04._Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            String gearder = Console.ReadLine();
+
+
+            if (gearder == "f")
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Ms.");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+
+            }
+
+            else
+
+            {
+                if (age >= 16)
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Master");
+                }
+
+            }
+
+        }
+    }
+}
