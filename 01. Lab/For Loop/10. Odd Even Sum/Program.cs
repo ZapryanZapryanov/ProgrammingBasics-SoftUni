6b9941ff2ab51adfdcb60faac4e7af78f1950ff4
zaprian1223 <zaprian1223@gmail.com>
2022-04-07 20:30:12 +0300

    Added a solution

---
+++ 01. Lab/For Loop/10. Odd Even Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 0;
+            int second = 0;
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    second += num;
+                }
+                else
+                {
+                    first += num;
+                }
+            }
+
+            if (first == second)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {second}");
+            }
+
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(first - second) }");
+
+            }
+        }
+    }
+}
