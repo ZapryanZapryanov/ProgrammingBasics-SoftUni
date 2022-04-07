8fe4fc15ccaa9606ddb20fae5c4e0aabb5d45b6b
zaprian1223 <zaprian1223@gmail.com>
2022-04-07 20:29:53 +0300

    Added a solution

---
+++ 01. Lab/For Loop/09. Left and Right Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _09._Left_and_Right_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            int finalyPrice2 = 0;
+            int finaly = 0;
+            int sum = 0;
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                int number1 = int.Parse(Console.ReadLine());
+                finaly += number1;
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                int number2 = int.Parse(Console.ReadLine());
+                finalyPrice2 += number2;
+            }
+            if (finalyPrice2 == finaly)
+            {
+                sum = finalyPrice2;
+                Console.WriteLine($"Yes, sum = {sum}");
+            }
+            else
+            {
+                double sell = finalyPrice2 - finaly;
+                Console.WriteLine($"No, diff = {Math.Abs(sell)}");
+            }
+
+        }
+    }
+}
