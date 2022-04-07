0f7f44d43e8f4473abfce03b9f29f47f807cbf7b
zaprian1223 <zaprian1223@gmail.com>
2022-04-07 20:29:34 +0300

    Added a solution

---
+++ 01. Lab/For Loop/08. Number sequence/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08._Number_sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+            for (int i = 1; i <= n; i++)
+            {
+                int numbers = int.Parse(Console.ReadLine());
+
+                if (numbers > maxNum)
+                {
+                    maxNum = numbers;
+                }
+                if (numbers < minNum)
+                {
+                    minNum = numbers;
+                }
+            }
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");
+        }
+    }
+}
