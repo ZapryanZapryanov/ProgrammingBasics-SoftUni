fe09c55c15b516fef52e2792b5b0d95141f5bddd
zaprian1223 <zaprian1223@gmail.com>
2022-04-07 20:28:09 +0300

    Added a solution

---
+++ 01. Lab/For Loop/04. Even Powers of 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Even_Powers_of_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= num; i += 2)
+            {
+
+                Console.WriteLine(Math.Pow(2, i));
+
+            }
+        }
+    }
+}
