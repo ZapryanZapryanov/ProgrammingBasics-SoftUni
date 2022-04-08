e4b744766a6379f04a22577bfe991ffce275a528
zaprian1223 <zaprian1223@gmail.com>
2022-04-08 20:18:34 +0300

    Added a solution

---
+++ 01. Lab/While Loop/08. Graduation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            double final = 0;
+            double evaluation = 1;
+            double torn = 0;
+
+
+            while (evaluation <= 12)
+            {
+                double currEvaluation = double.Parse(Console.ReadLine());
+
+
+                if (currEvaluation < 4)
+                {
+                    torn++;
+
+                    if (torn == 2)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {evaluation} grade");
+                        break;
+                    }
+                    continue;
+                }
+
+
+
+                evaluation++;
+                final += currEvaluation;
+
+            }
+            double all = final / 12;
+            if (torn < 2)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {all:f2}");
+            }
+
+        }
+    }
+}
