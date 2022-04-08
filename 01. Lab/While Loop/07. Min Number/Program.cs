16df9eaf7beacbebfbdae8d8c2bee7c3aca5aa04
zaprian1223 <zaprian1223@gmail.com>
2022-04-08 20:18:15 +0300

    Added a solution

---
+++ 01. Lab/While Loop/07. Min Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int numMin = int.MaxValue;
+
+            while (number != "Stop")
+            {
+
+                int numbersss = int.Parse(number);
+
+                if (numbersss < numMin)
+                {
+                    numMin = numbersss;
+                }
+                number = Console.ReadLine();
+
+
+
+            }
+            Console.WriteLine(numMin);
+        }
+    }
+}
