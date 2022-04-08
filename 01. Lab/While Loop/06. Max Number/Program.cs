62295aaa375c4a2235d0db879445760a9005e80f
zaprian1223 <zaprian1223@gmail.com>
2022-04-08 20:17:56 +0300

    Added a solution

---
+++ 01. Lab/While Loop/06. Max Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int numMin = int.MinValue;
+            while (number != "Stop")
+            {
+
+                int numbersss = int.Parse(number);
+
+                if (numbersss > numMin)
+                {
+                    numMin = numbersss;
+                }
+                number = Console.ReadLine();
+
+
+
+            }
+            Console.WriteLine(numMin);
+
+        }
+    }
+}
