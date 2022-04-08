d8e5a092d4bf1aaf3c1667c6601c238ed26790b6
zaprian1223 <zaprian1223@gmail.com>
2022-04-08 20:16:52 +0300

    Added a solution

---
+++ 01. Lab/While Loop/03. Sum Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+
+            int secondNum = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while (true)
+            {
+                sum += secondNum;
+                if (firstNum <= sum)
+                {
+                    Console.WriteLine(sum);
+                    break;
+                }
+
+                secondNum = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
