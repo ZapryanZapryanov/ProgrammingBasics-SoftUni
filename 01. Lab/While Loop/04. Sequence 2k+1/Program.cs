83e4029e3dcde5973895985e8e25c52181242e7c
zaprian1223 <zaprian1223@gmail.com>
2022-04-08 20:17:10 +0300

    Added a solution

---
+++ 01. Lab/While Loop/04. Sequence 2k+1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int finalNum = 1;
+
+            while (finalNum <= n)
+            {
+                Console.WriteLine(finalNum);
+                finalNum = finalNum * 2 + 1;
+
+
+            }
+
+        }
+    }
+}
