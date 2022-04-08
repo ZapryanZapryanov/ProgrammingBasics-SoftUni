bf22231aa4a721c60bd8a973f54d51049fe64f80
zaprian1223 <zaprian1223@gmail.com>
2022-04-08 20:17:34 +0300

    Added a solution

---
+++ 01. Lab/While Loop/05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05._Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string money = Console.ReadLine();
+
+            double price = 0;
+
+            while (money != "NoMoreMoney")
+            {
+
+                double currAmount = double.Parse(money);
+                if (currAmount < 0)
+                {
+                    Console.WriteLine($"Invalid operation!");
+                    break;
+                }
+                price += currAmount;
+                Console.WriteLine($"Increase: {currAmount:f2}");
+                money = Console.ReadLine();
+
+            }
+
+            Console.WriteLine($"Total: {price:f2}");
+        }
+    }
+}
