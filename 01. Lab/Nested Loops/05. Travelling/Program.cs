be904e0c90f1457c5a29834da01652d3618c8dfa
zaprian1223 <zaprian1223@gmail.com>
2022-04-09 17:42:49 +0300

    Added a solution

---
+++ 01. Lab/Nested Loops/05. Travelling/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string distinace = Console.ReadLine();
+
+            while (distinace != "End")
+            {
+                double money = double.Parse(Console.ReadLine());
+                double currMoney = 0;
+                while (currMoney < money)
+                {
+                    currMoney += double.Parse(Console.ReadLine());
+
+                }
+                Console.WriteLine($"Going to {distinace}!");
+                distinace = Console.ReadLine();
+            }
+        }
+    }
+}
