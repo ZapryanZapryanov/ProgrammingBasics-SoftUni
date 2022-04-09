99f73832a1327f4b31459182334a8195f6131e27
zaprian1223 <zaprian1223@gmail.com>
2022-04-09 17:42:19 +0300

    Added a solution

---
+++ 01. Lab/Nested Loops/03. Combinations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = 0;
+            for (int i = 0; i <= n; i++)
+            {
+
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int a = 0; a <= n; a++)
+                    {
+                        if (i + j + a == n)
+                        {
+                            k++;
+
+
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(k);
+        }
+    }
+}
