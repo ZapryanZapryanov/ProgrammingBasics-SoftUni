9ff12902fec6ba09f8622b67812a68f3e413c404
zaprian1223 <zaprian1223@gmail.com>
2022-04-10 16:20:07 +0300

    Added a solution

---
+++ 02. Excercise/First Steps In Coding/05. Supplies for School/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05._Supplies_for_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPens = int.Parse(Console.ReadLine());
+            int countMarkers = int.Parse(Console.ReadLine());
+            int liters = int.Parse(Console.ReadLine());
+            int percend = int.Parse(Console.ReadLine());
+
+            double pens = countPens * 5.80;
+            double markers = countMarkers * 7.20;
+            double lit = liters * 1.20;
+            double allMaterials = pens + markers + lit;
+
+            double sumAfterDiskount = allMaterials - (percend / 100.0) * allMaterials;
+            Console.WriteLine(sumAfterDiskount);
+        }
+    }
+}
