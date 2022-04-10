d1f55459b5af215c3be4d59eea34dedd1a077fe5
zaprian1223 <zaprian1223@gmail.com>
2022-04-10 16:20:58 +0300

    Added a solution

---
+++ 02. Excercise/First Steps In Coding/08. Basketball Equipment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int taxPerYear = int.Parse(Console.ReadLine());
+            double trainersPrice = taxPerYear - 0.4 * taxPerYear;
+            double team = trainersPrice - 0.2 * trainersPrice;
+            double ballPrice = team / 4;
+            double accessories = ballPrice / 5;
+            double totalCost = taxPerYear + trainersPrice + team + ballPrice + accessories;
+            Console.WriteLine(totalCost);
+
+        }
+    }
+}
