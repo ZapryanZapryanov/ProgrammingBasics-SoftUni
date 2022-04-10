3eb9ce75dfd1efe5204b9b4369d1e95449e702c0
zaprian1223 <zaprian1223@gmail.com>
2022-04-10 16:19:52 +0300

    Added a solution

---
+++ 02. Excercise/First Steps In Coding/04. Vacation Books List/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPages = int.Parse(Console.ReadLine());
+            int pages = int.Parse(Console.ReadLine());
+            int numberDay = int.Parse(Console.ReadLine());
+
+            int hoursBook = numberPages / pages;
+            int hoursDay = hoursBook / numberDay;
+            Console.WriteLine(hoursDay);
+        }
+    }
+}
