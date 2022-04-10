4af36569b9e2e4e8dfa27c9c306ce21ed2285a86
zaprian1223 <zaprian1223@gmail.com>
2022-04-10 16:19:35 +0300

    Added a solution

---
+++ 02. Excercise/First Steps In Coding/03. Deposit Calculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositAmount = double.Parse(Console.ReadLine());
+            int depositMothers = int.Parse(Console.ReadLine());
+            double interestRate = double.Parse(Console.ReadLine());
+
+            double interest = depositAmount * (interestRate / 100.0);
+            double interestMothers = interest / 12;
+            double total = depositAmount + depositMothers * interestMothers;
+            Console.WriteLine(total);
+        }
+    }
+}
