c2eeefa846ef77e3dd3d5a83506a98018280615e
zaprian1223 <zaprian1223@gmail.com>
2022-04-10 16:20:41 +0300

    Added a solution

---
+++ 02. Excercise/First Steps In Coding/07. Food Delivery/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07._Food_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberChikens = int.Parse(Console.ReadLine());
+            int numberFish = int.Parse(Console.ReadLine());
+            int numberVegitarian = int.Parse(Console.ReadLine());
+
+            double chikens = numberChikens * 10.35;
+            double fish = numberFish * 12.40;
+            double vegitarian = numberVegitarian * 8.15;
+
+            double totalPriceMenus = chikens + fish + vegitarian;
+            double dessert = 0.2 * totalPriceMenus;
+            double allMoney = totalPriceMenus + dessert + 2.50;
+            Console.WriteLine(allMoney);
+        }
+    }
+}
