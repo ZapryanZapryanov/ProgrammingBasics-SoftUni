18723a8cc527237ce7ebd5a1dec9fcc07d47b9b0
zaprian1223 <zaprian1223@gmail.com>
2022-04-10 16:21:15 +0300

    Added a solution

---
+++ 02. Excercise/First Steps In Coding/09. Fish Tank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09._Fish_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenghtCm = int.Parse(Console.ReadLine());
+            int widthCm = int.Parse(Console.ReadLine());
+            int heightCm = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+
+            double aquarium = lenghtCm * widthCm * heightCm;
+            double liteel = aquarium / 1000;
+            double housProcent = procent / 100.0;
+            double allLitters = liteel * (1 - housProcent);
+            Console.WriteLine(allLitters);
+        }
+    }
+}
