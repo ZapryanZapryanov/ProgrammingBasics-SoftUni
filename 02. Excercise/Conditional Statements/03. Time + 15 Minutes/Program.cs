33fd0075bdf3f7e931a183d70c2d6aa3766c42c1
zaprian1223 <zaprian1223@gmail.com>
2022-04-11 18:17:16 +0300

    Added a solution

---
+++ 02. Excercise/Conditional Statements/03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03._Time___15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 15;
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours += 1;
+            }
+            if (hours >= 24)
+            {
+                hours = 0;
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
