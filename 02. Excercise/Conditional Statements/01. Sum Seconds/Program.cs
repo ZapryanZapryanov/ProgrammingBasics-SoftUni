c2a2b3de95675bb611c681186434f1d93d23482b
zaprian1223 <zaprian1223@gmail.com>
2022-04-11 18:16:32 +0300

    Added a solution

---
+++ 02. Excercise/Conditional Statements/01. Sum Seconds/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01._Sum_Seconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTime = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int total = firstTime + secondTime + thirdTime;
+            int minutes = total / 60;
+            int second = total % 60;
+            if (second < 10)
+            {
+                Console.WriteLine($"{minutes}:{second:d2}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{second:d2}");
+            }
+        }
+    }
+}
