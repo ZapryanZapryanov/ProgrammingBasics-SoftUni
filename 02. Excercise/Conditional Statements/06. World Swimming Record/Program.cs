24f3c1e7c8ef8bce19f53b5d43eb55826237a6d3
zaprian1223 <zaprian1223@gmail.com>
2022-04-11 18:18:02 +0300

    Added a solution

---
+++ 02. Excercise/Conditional Statements/06. World Swimming Record/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double secondRecord = double.Parse(Console.ReadLine());
+            double meter = double.Parse(Console.ReadLine());
+            double second = double.Parse(Console.ReadLine());
+
+            double second15m = Math.Floor(meter / 15) * 12.5;
+            double allTime = meter * second + second15m;
+
+
+            if (secondRecord > allTime)
+            {
+                double finish = allTime;
+                Console.WriteLine($"Yes, he succeeded! The new world record is {finish:f2} seconds.");
+            }
+
+
+            else
+            {
+
+                double recordWin = allTime - secondRecord;
+                Console.WriteLine($"No, he failed! He was {recordWin:f2} seconds slower.");
+
+            }
+        }
+    }
+}
