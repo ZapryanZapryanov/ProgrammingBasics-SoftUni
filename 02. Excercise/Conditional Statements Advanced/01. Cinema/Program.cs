3ad124b93632447a7e2371b509277d12ae5237ab
zaprian1223 <zaprian1223@gmail.com>
2022-04-12 18:03:14 +0300

    Added a solution

---
+++ 02. Excercise/Conditional Statements Advanced/01. Cinema/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const double premiere = 12.00;
+            const double normal = 7.50;
+            const double discount = 5.00;
+
+            string project = Console.ReadLine();
+            int numLine = int.Parse(Console.ReadLine());
+            int numCollons = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch (project)
+            {
+                case "Premiere":
+                    price = premiere * numLine * numCollons;
+                    Console.WriteLine($"{price:f2} leva");
+                    break;
+                case "Normal":
+                    price = normal * numLine * numCollons;
+                    Console.WriteLine($"{price:f2} leva");
+                    break;
+                case "Discount":
+                    price = Discount * numLine * numCollons;
+                    Console.WriteLine($"{price:f2} leva");
+                    break;
+
+            }
+        }
+    }
+}
