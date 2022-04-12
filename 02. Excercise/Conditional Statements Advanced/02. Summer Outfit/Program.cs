06bbe32cfbd54aca666dcd9080ae6b751f1c1dee
zaprian1223 <zaprian1223@gmail.com>
2022-04-12 18:03:36 +0300

    Added a solution

---
+++ 02. Excercise/Conditional Statements Advanced/02. Summer Outfit/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace _02._Summer_Outfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gradus = double.Parse(Console.ReadLine());
+            string time = Console.ReadLine();
+            string outfit = "";
+            string outfit2 = "";
+
+            if (gradus >= 10 && gradus <= 18)
+            {
+                if (time == "Morning")
+                {
+                    outfit = "Sweatshirt";
+                    outfit2 = "Sneakers";
+                }
+                else if (time == "Afternoon")
+                {
+                    outfit = "Shirt";
+                    outfit2 = "Moccasins";
+                }
+                else
+                {
+                    outfit = "Shirt";
+                    outfit2 = "Moccasins";
+                }
+            }
+            else if (gradus <= 24)
+            {
+                if (time == "Morning")
+                {
+                    outfit = "Shirt";
+                    outfit2 = "Moccasins";
+                }
+
+                else if (time == "Afternoon")
+                {
+                    outfit = "T-Shirt";
+                    outfit2 = "Sandals";
+                }
+                else
+                {
+                    outfit = "Shirt";
+                    outfit2 = "Moccasins";
+                }
+            }
+            else
+            {
+                if (time == "Morning")
+                {
+                    outfit = "T-Shirt";
+                    outfit2 = "Sandals";
+                }
+                else if (time == "Afternoon")
+                {
+                    outfit = "Swim Suit";
+                    outfit2 = "Barefoot";
+                }
+                else
+                {
+                    outfit = "Shirt";
+                    outfit2 = "Moccasins";
+                }
+
+            }
+            Console.WriteLine($"It's {gradus} degrees, get your {outfit} and {outfit2}.");
+
+        }
+    }
+}
