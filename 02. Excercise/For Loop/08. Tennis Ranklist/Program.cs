1514ee0a7f24d1fc7523de205a407195913dc16d
zaprian1223 <zaprian1223@gmail.com>
2022-04-13 18:14:47 +0300

    Added a solution

---
+++ 02. Excercise/For Loop/08. Tennis Ranklist/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08._Tennis_Ranklist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numTornaments = int.Parse(Console.ReadLine());
+            int numToken = int.Parse(Console.ReadLine());
+            int token = 0;
+            int final = 0;
+
+
+            for (int i = 0; i < numTornaments; i++)
+            {
+                string etap = Console.ReadLine();
+                if (etap == "F")
+                {
+                    token += 1200;
+                }
+                else if (etap == "W")
+                {
+                    token += 2000;
+                    final++;
+                }
+                else if (etap == "SF")
+                {
+                    token += 720;
+                }
+            }
+
+            double price = token / numTornaments;
+            double winTornaments = (double)final / numTornaments * 100;
+            double all = token + numToken;
+            Console.WriteLine($"Final points: {all}");
+            Console.WriteLine($"Average points: {Math.Ceiling(price)}");
+            Console.WriteLine($"{winTornaments:f2}%");
+        }
+    }
+}
