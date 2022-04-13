9666cbe2e6fda5241f41509c3d0634d4586bb997
zaprian1223 <zaprian1223@gmail.com>
2022-04-13 18:13:26 +0300

    Added a solution

---
+++ 02. Excercise/For Loop/04. Clever Lily/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04._Clever_Lily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double pricePeral = double.Parse(Console.ReadLine());
+            int play = int.Parse(Console.ReadLine());
+            double toys = 0;
+            double money = 0;
+            double totalMoney = 0;
+
+
+            for (int i = 1; i <= age; i++)
+            {
+                money += 5;
+                if (i % 2 == 0)
+                {
+                    totalMoney += money - 1;
+                }
+                else
+                {
+                    toys++;
+                }
+            }
+
+            double Pricetoys = toys * play;
+            double allMoney = Pricetoys + totalMoney;
+            if (pricePeral <= allMoney)
+            {
+                double exit = allMoney - pricePeral;
+                Console.WriteLine($"Yes! {exit:f2}");
+            }
+            else
+            {
+                double no = pricePeral - allMoney;
+                Console.WriteLine($"No! {no:f2}");
+            }
+        }
+    }
+}
