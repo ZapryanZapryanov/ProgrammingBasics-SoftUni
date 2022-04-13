5c9b33e1397ba011d00cb7723a9c5518aa0a337f
zaprian1223 <zaprian1223@gmail.com>
2022-04-13 18:13:47 +0300

    Added a solution

---
+++ 02. Excercise/For Loop/05. Salary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05._Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int brauzers = int.Parse(Console.ReadLine());
+            int jobs = int.Parse(Console.ReadLine());
+            int totalPrice = 0;
+
+            for (int i = 1; i <= brauzers; i++)
+            {
+                string brauzersName = Console.ReadLine();
+                if (brauzersName == "Facebook")
+                {
+                    totalPrice += 150;
+                }
+                else if (brauzersName == "Instagram")
+                {
+                    totalPrice += 100;
+                }
+
+                else if (brauzersName == "Reddit")
+                {
+                    totalPrice += 50;
+                }
+
+            }
+            double totalPriceFine = jobs - totalPrice;
+            if (totalPriceFine <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+
+                Console.WriteLine(totalPriceFine);
+            }
+        }
+    }
+}
