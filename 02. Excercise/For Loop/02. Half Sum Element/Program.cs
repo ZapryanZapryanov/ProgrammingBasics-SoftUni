214a7503e7bf1d85e8fae9fc3ff2301fd6112965
zaprian1223 <zaprian1223@gmail.com>
2022-04-13 18:12:50 +0300

    Added a solution

---
+++ 02. Excercise/For Loop/02. Half Sum Element/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02._Half_Sum_Element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNum = int.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum += number;
+                if (number > maxNum)
+                {
+                    maxNum = number;
+                }
+
+            }
+            int sum1 = sum - maxNum;
+            if (maxNum == sum1)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {maxNum}");
+            }
+            else
+            {
+                int diff = Math.Abs(maxNum - sum1);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {diff}");
+            }
+        }
+    }
+}
