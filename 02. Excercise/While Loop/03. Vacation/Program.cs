58a0ad3f2cad7cd82831f8c18648ed9e0e3311fa
zaprian1223 <zaprian1223@gmail.com>
2022-04-14 18:06:35 +0300

    Added a solution

---
+++ 02. Excercise/While Loop/03. Vacation/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyEx = double.Parse(Console.ReadLine());
+            double money = double.Parse(Console.ReadLine());
+
+            int days = 0;
+            int spendDays = 0;
+
+
+            while (spendDays != 5)
+            {
+                string input = Console.ReadLine();
+                double saveMoney = double.Parse(Console.ReadLine());
+
+                days++;
+                if (input == "save")
+                {
+                    money += saveMoney;
+                    spendDays = 0;
+
+                }
+                else if (input == "spend")
+                {
+
+                    spendDays++;
+                    if (money < saveMoney)
+                    {
+                        money = 0;
+                    }
+                    else
+                    {
+                        money -= saveMoney;
+                    }
+                }
+
+                if (money >= moneyEx)
+                {
+                    Console.WriteLine($"You saved the money for {days} days.");
+                    break;
+                }
+
+
+            }
+            if (spendDays == 5)
+            {
+                Console.WriteLine("You can't save the money.");
+                Console.WriteLine(days);
+            }
+
+        }
+    }
+}
