a4bf8262080b774b7fde9f1b71090cada242d986
zaprian1223 <zaprian1223@gmail.com>
2022-04-14 18:12:55 +0300

    Added a solution

---
+++ 02. Excercise/Nested Loops/05. Special Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            for (int num1 = 1; num1 <= 9; num1++)
+            {
+                for (int num2 = 1; num2 <= 9; num2++)
+                {
+                    for (int num3 = 1; num3 <= 9; num3++)
+                    {
+                        for (int num4 = 1; num4 <= 9; num4++)
+                        {
+
+                            if (n % num1 == 0 && n % num2 == 0 && n % num3 == 0 && n % num4 == 0)
+                            {
+                                Console.Write($"{num1}{num2}{num3}{num4} ");
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
