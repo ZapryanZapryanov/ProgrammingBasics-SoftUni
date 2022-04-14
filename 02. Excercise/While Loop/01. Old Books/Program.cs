5f87422180af498f5d550ab89a788c8ea4c0354d
zaprian1223 <zaprian1223@gmail.com>
2022-04-14 18:05:59 +0300

    Added a solution

---
+++ 02. Excercise/While Loop/01. Old Books/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string book = Console.ReadLine();
+            int bookCuality = 0;
+            string books = Console.ReadLine();
+            while (books != "No More Books")
+            {
+
+                if (book == books)
+                {
+                    Console.WriteLine($"You checked {bookCuality} books and found it.");
+                    break;
+                }
+                bookCuality++;
+                books = Console.ReadLine();
+
+            }
+            if (books == "No More Books")
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {bookCuality} books.");
+            }
+        }
+    }
+}
