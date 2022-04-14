3137ae45a82219a5352243f46f148b62db160ea1
zaprian1223 <zaprian1223@gmail.com>
2022-04-14 18:12:39 +0300

    Added a solution

---
+++ 02. Excercise/Nested Loops/04. Train The Trainers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberJurry = int.Parse(Console.ReadLine());
+            string pressentation = Console.ReadLine();
+            double totalGreed = 0;
+            int count = 0;
+
+            while (pressentation != "Finish")
+            {
+                double oneGreed = 0;
+                for (int i = 1; i <= numberJurry; i++)
+                {
+                    double greed = double.Parse(Console.ReadLine());
+                    oneGreed += greed;
+                    count++;
+                    totalGreed += greed;
+                }
+                Console.WriteLine($"{pressentation} - {oneGreed / numberJurry:f2}.");
+
+                pressentation = Console.ReadLine();
+            }
+            double finishGreedYears = totalGreed / count;
+            Console.WriteLine($"Student's final assessment is {finishGreedYears:f2}.");
+        }
+    }
+}
