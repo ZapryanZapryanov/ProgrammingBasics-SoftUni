c63e1a2ca11935f8f1c2294914d10a075efc9ca4
zaprian1223 <zaprian1223@gmail.com>
2022-04-14 18:06:18 +0300

    Added a solution

---
+++ 02. Excercise/While Loop/02. Exam Preparation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace _02._Exam_Preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ocenka = int.Parse(Console.ReadLine());
+            string tasks = Console.ReadLine();
+
+
+            int allTasks = 0;
+            string outTaskt = "";
+            int won = 0;
+            int notOcenki = 0;
+
+
+            while (tasks != "Enough")
+            {
+                int ocenki = int.Parse(Console.ReadLine());
+                allTasks += ocenki;
+                won++;
+                outTaskt = tasks;
+                tasks = Console.ReadLine();
+                if (ocenki <= 4)
+                {
+                    notOcenki++;
+                    if (notOcenki == ocenka)
+                    {
+                        Console.WriteLine($"You need a break, {ocenka} poor grades.");
+                        break;
+                    }
+                }
+
+
+
+
+            }
+            if (tasks == "Enough")
+            {
+                double final = 1.0 * allTasks / won;
+                Console.WriteLine($"Average score: {final:f2}");
+                Console.WriteLine($"Number of problems: {won}");
+                Console.WriteLine($"Last problem: {outTaskt}");
+            }
+        }
+    }
+}
