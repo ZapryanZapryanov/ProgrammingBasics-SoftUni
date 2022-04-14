e53b141a24710f1c9c907d35bb0281e4260b5e28
zaprian1223 <zaprian1223@gmail.com>
2022-04-14 18:11:50 +0300

    Added a solution

---
+++ 02. Excercise/Nested Loops/01. Number Pyramid/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int row = 1;
+            int currNum = 1;
+            bool isEqual = false;
+            while (!isEqual)
+            {
+                for (int a = 1; a <= row; a++)
+                {
+                    Console.Write($"{currNum} ");
+                    currNum++;
+                    if (currNum > n)
+                    {
+                        isEqual = true;
+                        break;
+                    }
+                }
+                Console.WriteLine();
+                row++;
+            }
+
+        }
+    }
+}
