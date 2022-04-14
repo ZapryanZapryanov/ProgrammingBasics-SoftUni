47d2a06d15c07c333ac5e8f7d3acf4e54a6719bc
zaprian1223 <zaprian1223@gmail.com>
2022-04-14 18:07:36 +0300

    Added a solution

---
+++ 02. Excercise/While Loop/07. Moving/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int windt = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            string box = Console.ReadLine();
+            int all = windt * lenght * height;
+            int numberAll = 0;
+
+
+            while (box != "Done")
+            {
+                int numBox = int.Parse(box);
+                all -= numBox;
+                if (all < 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(all)} Cubic meters more.");
+                    break;
+                }
+
+                box = Console.ReadLine();
+            }
+            if (box == "Done")
+            {
+                Console.WriteLine($"{all} Cubic meters left.");
+            }
+        }
+    }
+}
