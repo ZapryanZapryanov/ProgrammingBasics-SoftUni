10d8e05d4b7fd636f14008d09f4c77c4981a49b5
zaprian1223 <zaprian1223@gmail.com>
2022-04-15 20:45:15 +0300

    Added a solution

---
+++ 03. More Exercises/Conditional Statements/03. Harvest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03._Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vineyard = double.Parse(Console.ReadLine());
+            var grape = double.Parse(Console.ReadLine());
+            var neededLiters = double.Parse(Console.ReadLine());
+            var workers = double.Parse(Console.ReadLine());
+
+            var harvest = vineyard * grape * 0.4;
+            var liters = harvest / 2.5;
+
+            if (liters >= neededLiters)
+            {
+                var leftWine = liters - neededLiters;
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(liters)} liters.");
+                Console.WriteLine($"{Math.Ceiling(leftWine)} liters left -> " +
+                    $"{Math.Ceiling(leftWine / workers)} liters per person.");
+            }
+            else
+            {
+                Console.WriteLine($"It will be a tough winter! " +
+                    $"More {Math.Floor(neededLiters - liters)} liters wine needed.");
+            }
+        }
+    }
+}
