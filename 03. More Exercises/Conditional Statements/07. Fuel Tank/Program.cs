07b3314680da0861f4d8b2074388f9ecdbbe3189
zaprian1223 <zaprian1223@gmail.com>
2022-04-15 20:46:25 +0300

    Added a solution

---
+++ 03. More Exercises/Conditional Statements/07. Fuel Tank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._Fuel_Tank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuel = Console.ReadLine().ToLower();
+            double amount = double.Parse(Console.ReadLine());
+
+            if (fuel == "diesel" || fuel == "gas" || fuel == "gasoline")
+            {
+                if (amount >= 25)
+                {
+                    Console.WriteLine($"You have enough {fuel}.");
+                }
+                else
+                {
+                    Console.WriteLine($"Fill your tank with {fuel}!");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Invalid fuel!");
+            }
+        }
+    }
+}
