763d14a8a878e2465be5546be7acf94a88882ea1
zaprian1223 <zaprian1223@gmail.com>
2022-04-15 20:44:39 +0300

    Added a solution

---
+++ 03. More Exercises/Conditional Statements/01. Pipes In Pool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01._Pipes_In_Pool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int scale = int.Parse(Console.ReadLine());
+            int pipe1 = int.Parse(Console.ReadLine());
+            int pipe2 = int.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+
+            double water = pipe1 * hours + pipe2 * hours;
+
+            if (water <= scale)
+            {
+                Console.WriteLine($"The pool is {(water / scale * 100):f2}% full. " +
+                $"Pipe 1: {(pipe1 * hours / water * 100):f2}%. Pipe 2: {(pipe2 * hours / water * 100):f2}%.");
+
+            }
+            else
+            {
+                Console.WriteLine($"For {hours:f2} hours the pool overflows with {water - scale:f2} liters.");
+            }
+        }
+    }
+}
