b9d17f7a2ee68e56d8d08b7d5c81e7082728418f
zaprian1223 <zaprian1223@gmail.com>
2022-04-16 14:12:43 +0300

    Added a solution

---
+++ 03. More Exercises/While-Loop/05. Average Number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05._Average_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+            }
+            sum /= n;
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
