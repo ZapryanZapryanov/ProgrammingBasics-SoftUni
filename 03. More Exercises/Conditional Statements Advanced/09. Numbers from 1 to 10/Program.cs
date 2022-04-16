4db250b50fb8c5dac0bd94704c3f14a74eccf0c1
zaprian1223 <zaprian1223@gmail.com>
2022-04-16 14:10:52 +0300

    Added a solution

---
+++ 03. More Exercises/Conditional Statements Advanced/09. Numbers from 1 to 10/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _09._Numbers_from_1_to_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+
+            while (n <= 10)
+            {
+                Console.WriteLine(n++);
+            }
+        }
+    }
+}
