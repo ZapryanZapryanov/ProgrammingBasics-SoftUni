60c56acd762d95b7edc164220ad56dfdc34df5f7
zaprian1223 <zaprian1223@gmail.com>
2022-04-16 14:08:36 +0300

    Added a solution

---
+++ 03. More Exercises/Conditional Statements Advanced/02. Bike Race/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _02._Bike_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numJuniors = int.Parse(Console.ReadLine());
+            int numSeniors = int.Parse(Console.ReadLine());
+            string terrain = Console.ReadLine();
+
+            double allBikers = numJuniors + numSeniors;
+            double price = 0;
+
+            if (terrain == "trail")
+            {
+                price = (numJuniors * 5.5) + (numSeniors * 7);
+                price *= 0.95;
+            }
+            if (terrain == "cross-country")
+            {
+                price = (numJuniors * 8) + (numSeniors * 9.50);
+                price *= 0.95;
+            }
+            if (terrain == "downhill")
+            {
+                price = (numJuniors * 12.25) + (numSeniors * 13.75);
+                price *= 0.95;
+            }
+            if (terrain == "road")
+            {
+                price = (numJuniors * 20) + (numSeniors * 21.50);
+                price *= 0.95;
+            }
+            else if (terrain == "cross-country" && allBikers >= 50)
+            {
+                price = (numJuniors * 8) + (numSeniors * 9.50);
+                price *= 0.95;
+                price *= 0.75;
+            }
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
