c1ddd64ffe768b8846fd52bac6fde2f353381878
zaprian1223 <zaprian1223@gmail.com>
2022-04-16 14:11:10 +0300

    Added a solution

---
+++ 03. More Exercises/Conditional Statements Advanced/10. Multiply by 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10._Multiply_by_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            while (number >= 0)
+            {
+                number *= 2;
+                Console.WriteLine($"Result: {number:f2}");
+                number = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Negative number!");
+        }
+    }
+}
