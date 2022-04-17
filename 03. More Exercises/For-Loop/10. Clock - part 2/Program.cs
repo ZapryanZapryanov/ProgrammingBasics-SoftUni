cec48d3f716994f66449c5058c739707b3f71825
zaprian1223 <zaprian1223@gmail.com>
2022-04-17 16:23:08 +0300

    Added a solution

---
+++ 03. More Exercises/For-Loop/10. Clock - part 2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _10._Clock___part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 23; i++)
+            {
+                for (int b = 0; b <= 59; b++)
+                {
+                    for (int c = 0; c <= 59; c++)
+                    {
+                        Console.WriteLine($"{i} : {b} : {c}");
+                    }
+                }
+            }
+        }
+    }
+}
