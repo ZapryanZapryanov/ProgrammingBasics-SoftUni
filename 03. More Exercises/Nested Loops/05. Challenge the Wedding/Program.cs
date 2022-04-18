b45044671063aaa18b4f33ec173f80b98df0ccc7
zaprian1223 <zaprian1223@gmail.com>
2022-04-18 18:30:07 +0300

    Added a solution

---
+++ 03. More Exercises/Nested Loops/05. Challenge the Wedding/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05._Challenge_the_Wedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int men = int.Parse(Console.ReadLine());
+            int women = int.Parse(Console.ReadLine());
+            int tablesMax = int.Parse(Console.ReadLine());
+            int tableCounter = 0;
+
+            for (int m = 1; m <= men; m++)
+            {
+                for (int f = 1; f <= women; f++)
+                {
+                    Console.Write($"({m} <-> {f}) ");
+                    tableCounter++;
+                    if (tableCounter == tablesMax)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
