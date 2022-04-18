431623ed884c05b9cbe7a35a8fec97e0e55dc7b3
zaprian1223 <zaprian1223@gmail.com>
2022-04-18 18:31:34 +0300

    Added a solution

---
+++ 03. More Exercises/Nested Loops/10. Profit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _10._Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int oneLevCoins = int.Parse(Console.ReadLine());
+            int twoLevCoins = int.Parse(Console.ReadLine());
+            int fiveLevCoins = int.Parse(Console.ReadLine());
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int one = 0; one <= oneLevCoins; one++)
+            {
+                for (int two = 0; two <= twoLevCoins; two++)
+                {
+                    for (int five = 0; five <= fiveLevCoins; five++)
+                    {
+                        if (one * 1 + two * 2 + five * 5 == sum)
+                        {
+                            Console.WriteLine($"{one} * 1 lv. + {two} * 2 lv. + {five} * 5 lv. = {sum} lv.");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
