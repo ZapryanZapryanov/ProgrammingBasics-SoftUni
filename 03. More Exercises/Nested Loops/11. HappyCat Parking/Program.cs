67b389eb0d399ad6a91a5dd3e5fc63334cd2f462
zaprian1223 <zaprian1223@gmail.com>
2022-04-18 18:31:56 +0300

    Added a solution

---
+++ 03. More Exercises/Nested Loops/11. HappyCat Parking/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _11._HappyCat_Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+            double total = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                sum = 0;
+                for (int j = 01; j <= hours; j++)
+                {
+                    if (i % 2 == 0 && j % 2 == 0)
+                    {
+                        sum += 1;
+                        total += 1;
+                    }
+                    if (i % 2 != 0 && j % 2 != 0)
+                    {
+                        sum += 1;
+                        total += 1;
+                    }
+                    if (i % 2 == 0 && j % 2 != 0)
+                    {
+                        sum += 2.50;
+                        total += 2.50;
+                    }
+                    if (i % 2 != 0 && j % 2 == 0)
+                    {
+                        sum += 1.25;
+                        total += 1.25;
+                    }
+                }
+                Console.WriteLine($"Day: {i} - {sum:f2} leva");
+            }
+            Console.WriteLine($"Total: {total:f2} leva");
+
+        }
+    }
+}
