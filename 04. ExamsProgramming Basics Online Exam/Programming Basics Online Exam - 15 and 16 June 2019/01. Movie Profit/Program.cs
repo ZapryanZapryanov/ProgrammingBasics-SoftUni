402376493e2fa562f8463572b1019335d319e3ed
zaprian1223 <zaprian1223@gmail.com>
2022-04-19 18:30:17 +0300

    Added a solution

---
+++ 04. ExamsProgramming Basics Online Exam/Programming Basics Online Exam - 15 and 16 June 2019/01. Movie Profit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Movie_Profit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int tickets = int.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+            int studioPercent = int.Parse(Console.ReadLine());
+
+            double ticketsPerDay = tickets * ticketPrice;
+            double totalIncome = ticketsPerDay * days;
+            double tax = totalIncome * studioPercent / 100;
+
+            Console.WriteLine($"The profit from the movie {movie} is {totalIncome - tax:f2} lv.");
+        }
+    }
+}
