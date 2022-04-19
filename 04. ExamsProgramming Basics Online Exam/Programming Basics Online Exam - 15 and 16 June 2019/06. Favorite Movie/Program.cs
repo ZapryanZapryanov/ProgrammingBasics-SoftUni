bf494a043496feb704092022d624496a05c9aab0
zaprian1223 <zaprian1223@gmail.com>
2022-04-19 18:33:02 +0300

    Added a solution

---
+++ 04. ExamsProgramming Basics Online Exam/Programming Basics Online Exam - 15 and 16 June 2019/06. Favorite Movie/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _06._Favorite_Movie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movie = Console.ReadLine();
+
+            int bestMovieScore = int.MinValue;
+            string bestMovie = string.Empty;
+            int count = 0;
+            while (movie != "STOP")
+            {
+                count++;
+
+                int totalP = 0;
+
+                foreach (var letter in movie)
+                {
+                    if (char.IsLower(letter))
+                    {
+                        totalP += (int)letter - movie.Length * 2;
+                    }
+                    else if (char.IsUpper(letter))
+                    {
+                        totalP += (int)letter - movie.Length;
+                    }
+                    else
+                    {
+                        totalP += (int)letter;
+                    }
+                }
+
+                int totalPoints = totalP;
+
+                if (totalPoints > bestMovieScore)
+                {
+                    bestMovieScore = totalPoints;
+                    bestMovie = movie;
+                }
+                if (count == 7)
+                {
+                    Console.WriteLine("The limit is reached.");
+                    break;
+                }
+                movie = Console.ReadLine();
+            }
+
+            Console.WriteLine($"The best movie for you is {bestMovie} with {bestMovieScore} ASCII sum.");
+        }
+    }
+}
