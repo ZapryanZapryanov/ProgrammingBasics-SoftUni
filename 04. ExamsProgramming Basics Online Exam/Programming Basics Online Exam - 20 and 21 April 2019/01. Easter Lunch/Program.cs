acbf003a1b8fc619c37e207e111792055a05ad7e
zaprian1223 <zaprian1223@gmail.com>
2022-04-20 17:38:57 +0300

    Added a solution

---
+++ 04. ExamsProgramming Basics Online Exam/Programming Basics Online Exam - 20 and 21 April 2019/01. Easter Lunch/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Easter_Lunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double bread = 3.2;
+            double eggs = 4.35; 
+            double biscuits = 5.4;
+            double paint = 0.15; 
+
+            int breadCount = int.Parse(Console.ReadLine());
+            int eggsCount = int.Parse(Console.ReadLine());
+            int biscuitsKgs = int.Parse(Console.ReadLine());
+
+            double totalSum = bread * breadCount + eggs * eggsCount + biscuits * biscuitsKgs + eggsCount * 12 * paint;
+
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
