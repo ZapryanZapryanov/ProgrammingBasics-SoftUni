ea64f18425a19ec87a2783600647777d00aace2a
zaprian1223 <zaprian1223@gmail.com>
2022-04-21 18:27:59 +0300

    Added a solution

---
+++ 04. ExamsProgramming Basics Online Exam/Programming Basics Online Exam - 6 and 7 April 2019/02. GodzillaKong/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._GodzillaKong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budgeFilm = double.Parse(Console.ReadLine());
+            int numberStatist = int.Parse(Console.ReadLine());
+            double clothWonStatist = double.Parse(Console.ReadLine());
+            double dekor = budgeFilm * 0.10;
+            double sumCloth = numberStatist * clothWonStatist;
+            if (numberStatist > 150)
+            {
+                sumCloth -= 0.10 * sumCloth;
+
+            }
+            double action = dekor + sumCloth;
+            if (action <= budgeFilm)
+            {
+                double finaly = budgeFilm - action;
+                Console.WriteLine("Action!");
+                Console.WriteLine($"Wingard starts filming with {finaly:f2} leva left.");
+
+            }
+            else
+            {
+                double finaly1 = Math.Abs(action - budgeFilm);
+                Console.WriteLine("Not enough money!");
+                Console.WriteLine($"Wingard needs {finaly1:f2} leva more.");
+            }
+        }
+    }
+}
