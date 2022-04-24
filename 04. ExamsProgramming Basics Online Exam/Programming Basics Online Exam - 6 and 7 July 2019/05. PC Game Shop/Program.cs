9db2c2bad2482da4749f946c17a250a7da95e559
zaprian1223 <zaprian1223@gmail.com>
2022-04-24 21:01:51 +0300

    Added a solution

---
+++ 04. ExamsProgramming Basics Online Exam/Programming Basics Online Exam - 6 and 7 July 2019/05. PC Game Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._PC_Game_Shop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int heartstone = 0;
+            int fornine = 0;
+            int overwatch = 0;
+            int others = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                string nameGames = Console.ReadLine();
+                switch (nameGames)
+                {
+                    case "Hearthstone":
+                        heartstone++;
+                        break;
+                    case "Fornite":
+                        fornine++;
+                        break;
+                    case "Overwatch":
+                        overwatch++;
+                        break;
+                    default:
+                        others++;
+                        break;
+                }
+            }
+            double percendH = (double)heartstone / n * 100.0;
+            double percendF = (double)fornine / n * 100.0;
+            double percendo = (double)overwatch / n * 100.0;
+            double percendOthers = (double)others / n * 100.0;
+            Console.WriteLine($"Hearthstone - {percendH:f2}%");
+            Console.WriteLine($"Fornite - {percendF:f2}%");
+            Console.WriteLine($"Overwatch - {percendo:f2}%");
+            Console.WriteLine($"Others - {percendOthers:f2}%");
+        }
+    }
+}
